ff3e610f15c3d0fff56493a1aa7e814b10cc5856
anasmhmd <116856719+anasmhmd@users.noreply.github.com>
2024-01-02 22:55:07 +0530

    Add files via upload

---
+++ longest_word.py
@@ -0,0 +1,7 @@
+def longest_word(word):
+    word=max(word,key_len)
+    print("Longest word:",word)
+    print("length of the longest word is:",len(word))
+a=["book","calculator","pen","pencil","bag"]
+longest_word(a)
+
